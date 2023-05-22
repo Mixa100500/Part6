31bff98a84764e8ad14842bb24d13bd85cb79344
Mixa100500 <popffggrr@gmail.com>
2023-05-23 00:25:14 +0300

    6.15

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteServise from './service/anecdotes'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteServise
+    .getAll().then(anecdotes => 
+      dispatch(setAnecdotes(anecdotes))
+    )
+    
+  }, [dispatch])
 
 
   return (
--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -4,9 +4,6 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './reducers/store'
 
-store.subscribe(() => {
-  console.log(store.getState())
-})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
